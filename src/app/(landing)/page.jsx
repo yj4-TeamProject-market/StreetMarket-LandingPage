890faec2a0fbbaf453d3571009f54ed780726d12
etoilebonbon <violetjardin@daum.net>
2024-04-19 16:16:48 +0900

    study

--- src/app/(landing)/page.jsx
+++ src/app/(landing)/page.jsx
@@ -1,6 +1,41 @@
+import Slide from "@/components/Slide/Slide";
+
+const slideContents = [
+  {
+    title: "Market Vendor",
+    sub: "플리마켓 기본 셋팅 물품 제공",
+    contents:
+      "테이블, 스탠드, 마네킹 등 제공해드립니다. 오직 고객과 상품판매에만 집중해주세요!",
+    image: "MarketVendor.svg",
+  },
+
+  {
+    title: "Queickly and Easily",
+    sub: "빠르고 간편한 플리마켓 참가신청",
+    contents:
+      "안전한 마켓 참가를 위해 불필요한 개인정보는요구하지 않습니다.단, 판매제품의 안전과 인증을 위해별도의 정보요청할 수 있습니다.",
+    image: "QueicklyAndEasily.svg",
+  },
+
+  {
+    title: "Promotional Alert",
+    sub: "스트릿마켓 친구추가 고객에게마켓 알림톡 전송",
+    contents:
+      "스트릿마켓을 친구추가한 분들께플리마켓 알림톡 전송을 합니다.알림톡 서비스로 홍보효과를 누려보세요!.",
+    image: "PromotionalAlert.svg",
+  },
+];
+
+// const map2 = [];
+
+// for (var i = 0; i < slideContents.length; i++) {
+//   map2.push(<Slide obj={slideContents[i]} />);
+// }
+
+const map = slideContents.map((e) => {
+  return <Slide obj={e} />;
+});
 
 export default function LandingPage() {
-  return (
-    <div>메인페이지</div>
-  )
+  return <div>{map}</div>;
 }
--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,6 +1,19 @@
-
-export default function Slide() {
+export default function Slide({ obj }) {
   return (
-    <div>슬라이드임</div>
-  )
+    <div className="h-lvh flex flex-col justify-around px-[20px] bg-[#FB9E01]">
+      {/* 컨테이너 */}
+      <div className="flex flex-col pt-10 text-black">
+        {/* 타이틀 */}
+        <div className="w-full text-4xl mb-5 font-semibold">{obj.title}</div>
+        {/* 서브타이틀 */}
+        <div className="mb-2.5 text-[18px] font-medium">{obj.sub}</div>
+        {/* 콘텐츠 */}
+        <div className="text-[14px] mb-[38px]">{obj.contents}</div>
+      </div>
+      {/* 이미지 */}
+      <div className="w-full flex justify-center">
+        <img className="" src={obj.image} alt="" />
+      </div>
+    </div>
+  );
 }
