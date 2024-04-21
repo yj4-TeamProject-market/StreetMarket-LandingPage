8b109fac2c46bb892212212d2c70dc22cbea061b
Jin young <xmfkdnak98@gmail.com>
2024-04-22 00:59:00 +0900

    design: 중나좋체 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        custom: ['Joongnajoche', ],
       },
+      fontWeight: {
+        normal: 400,
+        bold: 700,
+      }
     },
   },
   plugins: [],
