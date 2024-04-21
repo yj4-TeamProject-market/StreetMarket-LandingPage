94f242750bb8b72287839cc2e7dd75159c88ba58
Jin young <xmfkdnak98@gmail.com>
2024-04-22 08:13:45 +0900

    feat: header와 footer html과 css 단순 추가

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,37 @@
 export default function Footer() {
   return (
-    <div>푸터임</div>
+    <footer className="w-full h-dvh">
+      <div>
+        <h2 className="">스트릿마켓 주의사항</h2>
+        <div>
+          <p>1. 스트릿마켓에서 운영 중 발생된 CS문제는 스트릿마켓이 응대를 합니다.</p>
+          <p>(단, 이 외 발생한 CS문제는 셀러 본인이 해결하셔야합니다.)</p>
+        </div>
+        <div>
+          <p>2. 현금영수증 문제 발생시 이는 스트릿마켓이 해결하지 않습니다.</p>
+          <p>(현금영수증 문제는 법적필수고지사항입니다. 이는 셀러에게 귀책사유가 되므로 스트릿마켓이 해결하지않습니다.)</p>
+        </div>
+      </div>
+      <div>
+        <h2 className="">STREETMARKET</h2>
+        <ul>
+          <li>(주)스트릿마켓</li>
+          <li>012-34-567890123</li>
+          <li>서울</li>
+        </ul>        
+      </div>
+      <div>
+        <h2 className="">CONTAC</h2>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <p>COPYRIGHT INFORMATION GOES HERE © 2017. ALL RIGHTS RESERVED.</p>
+      </div>
+    </footer>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 
-export default function Header() {
+export default function Header({ slideIndex }) {
+ 
+  const [isSlideFirst, setIsSlideFirst] = useState(true);
+
+  useEffect(()=>{
+    if(-slideIndex > 0) setIsSlideFirst(false);
+    else setIsSlideFirst(true)
+  }, [, slideIndex])
+
   return (
-    <div></div>
+    <header className="w-full h-[60px] px-[20px] fixed text-[#FFD700] z-50 duration-1000">
+      <div className="w-full h-full flex justify-between">
+        <div className="w-full relative ">
+          <div className="absolute top-1/2 -translate-y-1/2 left-0">
+            <h1 className="text-4xl">Street Market</h1>
+          </div>
+          <div className="bg-[#FFD700] absolute right-0 top-1/2 p-[10px] rounded-t-xl">
+            <button className="text-[14px] text-black">서비스 알림신청</button>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
+
+
+
