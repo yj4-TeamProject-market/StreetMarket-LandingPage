824f18555aa3ea8aadb35c8f9fb3f9143fb1b5f9
Jinyoung Choi <xmfkdnak98@gmail.com>
2024-04-22 13:24:33 +0900

    feat: header 레이아웃 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,41 @@
 import { useEffect, useState } from "react";
 
-export default function Header({ slideIndex }) {
+export default function Header({ slideIndex, setSlideIndex }) {
  
   const [isSlideFirst, setIsSlideFirst] = useState(true);
 
+  const style = {
+    background: isSlideFirst ? "" : "bg-black",
+    visible: isSlideFirst ? "visible opacity-0" : " opacity-100",
+    // overflowHidden : isSlideFirst ? "" : "overflow-hidden",
+    titleContainer: isSlideFirst ? "top-[100%]" : "top-[10%]",
+    title: isSlideFirst ? "text-[52px]" : "text-[38px] text-[#FFD700]",
+
+  }
+
   useEffect(()=>{
-    if(-slideIndex > 0) setIsSlideFirst(false);
+    if(slideIndex > 0) setIsSlideFirst(false);
     else setIsSlideFirst(true)
-  }, [, slideIndex])
+  }, [slideIndex])
+
+  const handleClick = () => {
+    setSlideIndex(0)
+  }
 
   return (
-    <header className="w-full h-[60px] px-[20px] fixed text-[#FFD700] z-50 duration-1000">
+    <header className={`w-full h-[60px] px-[20px] fixed z-50 duration-1000 ${style.background} ${style.overflowHidden} `}>
       <div className="w-full h-full flex justify-between">
         <div className="w-full relative ">
-          <div className="absolute top-1/2 -translate-y-1/2 left-0">
-            <h1 className="text-4xl">Street Market</h1>
+          <div className={`absolute left-0 ${style.titleContainer} duration-1000`}>
+            <h1 className={`${style.title} duration-1000`}>Street Market</h1>
           </div>
-          <div className="bg-[#FFD700] absolute right-0 top-1/2 p-[10px] rounded-t-xl">
-            <button className="text-[14px] text-black">서비스 알림신청</button>
+          <div className=" h-full overflow-hidden">
+            <div className={`absolute right-0 bottom-0 overflow-hidden duration-1000 ${style.visible}`}>
+              <button onClick={handleClick} className="bg-[#FFD700] px-4 pt-2 rounded-t-xl">신청하기</button>
+            </div>
           </div>
         </div>
       </div>
     </header>
   )
 }
-
-
-
