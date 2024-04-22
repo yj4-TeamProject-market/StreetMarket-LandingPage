b7f3d60b66fc359cf3ed2e6e068e3470559268b8
Jinyoung Choi <xmfkdnak98@gmail.com>
2024-04-22 12:08:26 +0900

    refactor: onScrollChange함수 직관적으로 수정

--- src/components/SlideContainer/SlideContainer.jsx
+++ src/components/SlideContainer/SlideContainer.jsx
@@ -18,10 +18,10 @@ export default function SlideContainer( {children, slideIndex, setSlideIndex} ) 
   },[container])
 
   const onScrollChange = (event) => {
-    if(event.deltaY < 0 && slideIndex < 0) {
-      setSlideIndex(prev => prev + 1);
-    } else if(event.deltaY > 0 && slideIndex > -slideLength) {
-      setSlideIndex(prev => prev - 1);
+    if(event.deltaY < 0 && slideIndex >= 0) {
+      setSlideIndex(prev => prev-1);
+    } else if(event.deltaY > 0 && slideIndex < slideLength-1) {
+      setSlideIndex(prev => prev+1);
     }
   }
   const handleWheel = throttel(onScrollChange, delay, inThrottle);
@@ -31,7 +31,7 @@ export default function SlideContainer( {children, slideIndex, setSlideIndex} ) 
       ref={container} 
       onWheel={handleWheel} 
       className={`w-full duration-1000 `}
-      style={{transform : `translateY(${slideIndex * 100}vh)`}}
+      style={{transform : `translateY(-${slideIndex * 100}vh)`}}
     >
       {children}
     </div>
