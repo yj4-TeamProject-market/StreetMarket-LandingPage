e21489942712ef5e088643e2044986bdaf4495df
YeonSung <150204668+koinoniays2@users.noreply.github.com>
2024-04-22 14:24:11 +0900

    [update] env

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 export async function connectToDatabase() {
     const connection = await mysql.createConnection({
-        host: "localhost",
-        user: "admin",
-        password: "1234",
-        database: "market"
+        host: process.env.NEXT_PUBLIC_HOST,
+        port: process.env.NEXT_PUBLIC_PORT,
+        user: process.env.NEXT_PUBLIC_USER,
+        password: process.env.NEXT_PUBLIC_PASS,
+        database: process.env.NEXT_PUBLIC_DATABASE
     });
     return connection;
 }
