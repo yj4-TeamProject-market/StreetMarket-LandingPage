294ed5ddba4595287ea725856feaef80f9eb6705
Jinyoung Choi <xmfkdnak98@gmail.com>
2024-04-22 14:22:59 +0900

    design: header 애니메이션 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,9 @@ export default function Header({ slideIndex, setSlideIndex }) {
   const [isSlideFirst, setIsSlideFirst] = useState(true);
 
   const style = {
-    background: isSlideFirst ? "" : "bg-black",
-    visible: isSlideFirst ? "visible opacity-0" : " opacity-100",
-    // overflowHidden : isSlideFirst ? "" : "overflow-hidden",
-    titleContainer: isSlideFirst ? "top-[100%]" : "top-[10%]",
+    height : isSlideFirst ? "h-0" : "h-[60px]",
+    titleContainer: isSlideFirst ? "top-[100%] translate-y-[80%] " : "top-[10%]",
     title: isSlideFirst ? "text-[52px]" : "text-[38px] text-[#FFD700]",
-
   }
 
   useEffect(()=>{
@@ -23,14 +20,14 @@ export default function Header({ slideIndex, setSlideIndex }) {
   }
 
   return (
-    <header className={`w-full h-[60px] px-[20px] fixed z-50 duration-1000 ${style.background} ${style.overflowHidden} `}>
+    <header className={`w-full h-[60px] px-[20px] fixed z-50 bg-black duration-1000 ${style.height} `}>
       <div className="w-full h-full flex justify-between">
         <div className="w-full relative ">
           <div className={`absolute left-0 ${style.titleContainer} duration-1000`}>
             <h1 className={`${style.title} duration-1000`}>Street Market</h1>
           </div>
           <div className=" h-full overflow-hidden">
-            <div className={`absolute right-0 bottom-0 overflow-hidden duration-1000 ${style.visible}`}>
+            <div className={`absolute right-0 bottom-0 overflow-hidden duration-1000`}>
               <button onClick={handleClick} className="bg-[#FFD700] px-4 pt-2 rounded-t-xl">신청하기</button>
             </div>
           </div>
